dbee10d03b1d72201c4a5878c04c3889eeba799c
Felipe Araujo Soares <felipe.soares@paxtecnologia.com.br>
2020-05-12 15:21:08 -0300

    requireUncached

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,14 @@
+/**
+ * Módulo que serve funções utilitárias para necessidades diversas.
+ */
+
 module.exports = {
-  capitalizeFirst: text => text.charAt(0).toUpperCase().concat(text.slice(1))
+  // primeira letra do texto em maiúsculo
+  capitalizeFirst: text => text.charAt(0).toUpperCase().concat(text.slice(1)),
+
+  // require de um módulo sem cache
+  requireUncached: module => {
+    delete require.cache[require.resolve(module)]
+    return require(module)
+  }
 }
