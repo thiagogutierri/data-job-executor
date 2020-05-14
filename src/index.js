8ee9aa81e8d6944b827dfc7c9054e7ff970125d0
Felipe Araujo Soares <felipe.soares@paxtecnologia.com.br>
2020-05-14 10:17:45 -0300

    Single execution scheduler

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const log = require('./logger')(__filename)
 const JobFactory = require('./factories/JobFactory')
+const SchedulerFactory = require('./factories/SchedulerFactory')
 
 function start () {
   log.info('Inicializando a aplicação...')
@@ -10,9 +11,16 @@ function start () {
 
   // Pegando o objeto de job
   const job = JobFactory.get(jobType)
-  job.execute()
-    .catch(err => log.error(err.stack))
-    .finally(process.exit)
+
+  const schedulerType = process.env.SCHEDULER || 'singleExecution'
+  const schedulerInterval = process.env.SCHEDULER_INTERVAL // A cada minuto
+
+  log.info('Scheduler do tipo %s para o intervalo %s sendo criado...', schedulerType, schedulerInterval)
+  const Scheduler = SchedulerFactory.get(schedulerType)
+
+  Scheduler.schedule(job, {
+    interval: schedulerInterval
+  })
 }
 
 // start do job
---
+++ src/schedulers/SingleExecution.js
@@ -0,0 +1,25 @@
+const Scheduler = require('./Scheduler')
+const log = require('../logger')(__filename)
+
+class SingleExecution extends Scheduler {
+  /**
+   * Schedule um Job que executará apenas uma vez.
+   * @param {Job} job O Job.
+   * @param {Object} options Objeto de parâmetros.
+   */
+  static async schedule (job, options) {
+    // loga
+    log.debug('Criando scheduler para o job %O', job)
+
+    // executa
+    let exitCode = 0
+    job.execute()
+      .catch(err => {
+        log.error('Erro na execução do job!', err.stack)
+        exitCode = 1
+      })
+      .finally(() => process.exit(exitCode))
+  }
+}
+
+module.exports = SingleExecution
